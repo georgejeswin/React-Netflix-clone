21dff238ed48a19fc39509a727a0e00bfae8e693
georgejeswin <jeswinmyladoor@gmail.com>
2020-11-15 04:19:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+// import logo from './logo.svg';
+import "./App.css";
+import requests from "./requests";
+import Banner from "./Banner";
+import Row from "./Row";
+import Nav from "./Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Acton Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomaceMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
